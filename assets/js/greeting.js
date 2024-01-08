3144c1eb7c8a55ed84c9d903ab4fa45a1946b75f
phuocn0302 <phuocnguyen230302@gmail.com>
2024-01-09 03:48:41 +0700

    use pywal color and change layout

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -5,7 +5,7 @@
 
 const today = new Date();
 const hour = today.getHours();
-const name = CONFIG.name;
+
 
 const gree1 = `${CONFIG.greetingNight}\xa0`;
 const gree2 = `${CONFIG.greetingMorning}\xa0`;
@@ -13,11 +13,11 @@ const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
 if (hour >= 23 || hour < 6) {
-	document.getElementById('greetings').innerText = gree1 + name;
+  document.getElementById("greetings").innerText = gree1 ;
 } else if (hour >= 6 && hour < 12) {
-	document.getElementById('greetings').innerText = gree2 + name;
+  document.getElementById("greetings").innerText = gree2 ;
 } else if (hour >= 12 && hour < 17) {
-	document.getElementById('greetings').innerText = gree3 + name;
+  document.getElementById("greetings").innerText = gree3 ;
 } else {
-	document.getElementById('greetings').innerText = gree4 + name;
+  document.getElementById("greetings").innerText = gree4 ;
 }
